1a41f7d0f9ff3f2ba2d1dfbf3fe1285c786ae165
RozanskiT <tomasz.rozanski95@gmail.com>
2020-03-13 14:34:21 +0100

    Create atlas and atlas tests

--- tests/test_atlas.py
+++ tests/test_atlas.py
@@ -1,6 +1,41 @@
 from vidmapy.kurucz import atlas
+import pytest 
 
-def test_sum():
-    assert 5 == atlas.sum(2,3)
-    assert 5. == atlas.sum(2.,3.)
+from unittest.mock import MagicMock
+
+@pytest.fixture
+def atlas_model():
+    parameters = MagicMock()
+    parameters.teff = 5777
+    parameters.logg = 4.44
+    parameters.metallicity = 0.0
+    return atlas.Atlas(parameters)
+
+def test_raport_state(atlas_model):
+    assert atlas_model.get_report() == "TEFF  =  5777\nLOGG  = 4.44\n[M/H] = 0.00"
+
+def test_set_variables_with_setters(atlas_model):
+    parameters = MagicMock()
+    parameters.teff = 5000
+    parameters.logg = 4.3
+    parameters.metallicity = 1.0
+
+    atlas_model.teff = parameters.teff
+    atlas_model.logg = parameters.logg
+    atlas_model.metallicity = parameters.metallicity
+
+    assert atlas_model.teff == parameters.teff
+    assert atlas_model.logg == parameters.logg
+    assert atlas_model.metallicity == parameters.metallicity
+
+def test_define_all_parameters_at_once(atlas_model):
+    parameters = MagicMock()
+    parameters.teff = 5000
+    parameters.logg = 4.3
+    parameters.metallicity = 1.0
+
+    atlas_model.parameters = parameters
     
+    assert atlas_model.teff == parameters.teff
+    assert atlas_model.logg == parameters.logg
+    assert atlas_model.metallicity == parameters.metallicity
--- vidmapy/kurucz/atlas.py
+++ vidmapy/kurucz/atlas.py
@@ -3,11 +3,41 @@
 """
 """
 
-def sum(a,b):
-    return a+b
+class Atlas:
+    def __init__(self, parameters):
+        self.parameters = parameters
+
+    def get_report(self):
+        return f"TEFF  = {self.parameters.teff:5.0f}\n"\
+               f"LOGG  = {self.parameters.logg:4.2f}\n"\
+               f"[M/H] = {self.parameters.metallicity:4.2f}"
+
+    @property
+    def teff(self):
+        return self.parameters.teff
+        
+    @property
+    def logg(self):
+        return self.parameters.logg
+
+    @property
+    def metallicity(self):
+        return self.parameters.metallicity  
+
+    @teff.setter
+    def teff(self, teff):
+        self.parameters.teff = teff
+
+    @logg.setter
+    def logg(self, logg):
+        self.parameters.logg = logg
+
+    @metallicity.setter
+    def metallicity(self, metallicity):
+        self.parameters.metallicity = metallicity  
+
 
 def main():
-    print("Hello world!")
     pass
 
 if __name__ == '__main__':
