27c50a6935321447d9dc9417cf210ed3d5b191d1
RozanskiT <tomasz.rozanski95@gmail.com>
2020-03-17 11:59:01 +0100

    Update main with expected typical usage

--- main.py
+++ main.py
@@ -1,18 +1,27 @@
 #!/usr/bin/env python3
 
-from vidmapy.kurucz import atlas
-from vidmapy.kurucz import parameters
-from vidmapy.kurucz import model
+from vidmapy.kurucz.atlas import Atlas
+from vidmapy.kurucz.parameters import Parameters
 
 import matplotlib.pyplot as plt
 
-def main():
-    p = parameters.Parameters()
+def run_atlas():
+    worker = Atlas()
+    m1 = worker.get_model(Parameters(8000., 4.0, 0.0, 2.0)) # from grid
+    m2a = worker.get_model(Parameters(8120., 4.0, 0.0, 2.0)) # computed
+    m2b = worker.get_model(Parameters(8130., 4.0, 0.0, 2.0)) # computed
+    m3 = worker.get_model(Parameters(8250., 4.0, 0.0, 2.0)) # from grid
 
-    # atlas_code = atlas.Atlas(p)
-    x = model.Model("/home/tr/repos/vidmapy/tests/test_data/model.txt")
+    plt.plot(m1.structure["RHOX"], m1.structure["T"],'k')
+
+    plt.plot(m2a.structure["RHOX"], m2a.structure["T"],'g')
+    plt.plot(m2b.structure["RHOX"], m2b.structure["T"],'b')
     
-    print("END")
+    plt.plot(m3.structure["RHOX"], m3.structure["T"],'k')
+    plt.show()
+
+def main():
+    run_atlas()
 
 if __name__ == '__main__':
     main()
